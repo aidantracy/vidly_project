b29ffad6dc5323cbe6ac80bf4b0ea0fc3308f0a0
Aidan Tracy <aidantracy@outlook.com>
2023-08-20 16:43:48 -0600

    added new user schema and model

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+const Joi = require("joi")
+
+const User = mongoose.model("User", new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    }
+}));
+
+
+// input validation
+function validateUser(new_user){
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(5).max(255).required(),
+        password: Joi.string().min(5).max(255).required()
+    });
+    return schema.validate(new_user)
+}
+
+
+exports.User = User;
+exports.validateUser = validateUser;
