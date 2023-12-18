40fcbddf87014f34b89ea17308dd96aefcbb99b4
trac0082 <aidantracy@outlook.com>
2023-12-17 20:04:57 -0700

    added lodash and password hashing

--- routers/users.js
+++ routers/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
@@ -14,17 +16,17 @@ router.post('/', async (req,res) => {
     if (user) return res.status(400).send("User Already Registered");
 
     // instantiate a new user
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User(_.pick(["name", "email", "password"]));
+
+    //hash user password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     
-    //save to database
+    //save user to database
     await user.save();
 
     //display on console
-    res.send(user);
+    res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
