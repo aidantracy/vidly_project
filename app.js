e19a5614d11f1ad8355dc4028eed9ecdb5ce5626
trac0082 <aidantracy@outlook.com>
2023-12-20 18:20:08 -0700

    Added user validation with passwords and emails

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require("express")
 const mongoose = require("mongoose")
-const app = express();
+const app = express()
 const genres = require("./routers/genres")
 const customers = require("./routers/customers")
 const movies = require("./routers/movies")
 const rentals = require("./routers/rentals")
 const users = require("./routers/users")
+const auth = require("./routers/auth")
 const Joi = require("joi")
 Joi.objectId = require("joi-objectid")(Joi);
 
@@ -14,12 +15,13 @@ const uri = "mongodb+srv://aidantracy:rootroot@vidlydemocluster.aew1dex.mongodb.
 
 
 // middleware
-app.use(express.json());
-app.use("/api/genres", genres);
+app.use(express.json())
+app.use("/api/genres", genres)
 app.use("/api/customers", customers)
 app.use("/api/movies", movies)
 app.use("/api/rentals", rentals)
 app.use("/api/users", users)
+app.use("/api/auth")
 
 
 const port = process.env.PORT || 3000;
